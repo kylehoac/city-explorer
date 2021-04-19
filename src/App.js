176a5d5ce31d75b900891618f2d906ba46da4c16
Kyle Hoac <hoackyle@gmail.com>
2021-04-19 15:51:27 -0700

    added static map and state name to my react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      searchQuery: '',
+      location: {},
+    }
+  };
+
+
+getLocation = async (e) => {
+  e.preventDefault();
+  const apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.searchQuery}&format=json`;
+  
+  const response = await axios.get(apiUrl);
+
+  this.setState({
+    location: response.data[0],
+  });
 }
 
-export default App;
+render() {
+  const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=<zoom>&size=${window.innerWidth}x300&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`
+  return(
+    <>
+      <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="Enter a city name"></input>
+    
+      <button onClick={this.getLocation}>Explore</button>
+      {this.state.location.place_id &&
+      <>
+        <h2> The city is: {this.state.location.display_name}</h2>
+        <img src={mapUrl} alt="location" />
+      </>
+        }
+    </>
+  )
+}
+}
